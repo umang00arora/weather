a1d7c47b20f78467375fdea26a6b3a5bcb33ce0b
umang00arora <umangarora810@gmail.com>
2023-08-25 10:11:32 +0530

    new

---
+++ src/components/WeatherApp/WeatherApp.js
@@ -0,0 +1,117 @@
+import React, { useEffect,useState } from 'react'
+import "./WeatherApp.css"
+import clear_icon from "../Assets/clear.png"
+import cloud_icon from "../Assets/cloud.png"
+import drizzle_icon from "../Assets/drizzle.png"
+import humidity_icon from "../Assets/humidity.png"
+import rain_icon from "../Assets/rain.png"
+import search_icon from "../Assets/search.png"
+import snow_icon from "../Assets/snow.png"
+import wind_icon from "../Assets/wind.png"
+
+
+const WeatherApp = () => {
+    let api_key="8ba6d0f314b9a4b415761fca96cfccdb";
+
+    const[wicon,setWicon]=useState(cloud_icon);
+
+    const search=async()=>{
+      //const che=false
+      const element=document.getElementsByClassName("cityInput")
+      if(element[0].value==="")
+      {
+        element[0].value="Delhi";
+      }
+
+      let url=`https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`
+      let response=await fetch(url);
+      let data=await response.json();
+      const humidity=document.getElementsByClassName("humidity-percent");
+      const wind=document.getElementsByClassName("wind-rate");
+      const temprature=document.getElementsByClassName("weather-temp");
+      const location=document.getElementsByClassName("weather-location");
+      const err=document.getElementsByClassName("err-msg");
+      if(data.cod===200){
+      err[0].innerHTML="";
+      humidity[0].innerHTML=data.main.humidity + " %";
+      wind[0].innerHTML=Math.floor(data.wind.speed) + " km/hr";
+      temprature[0].innerHTML=Math.floor(data.main.temp) + " °C";
+      location[0].innerHTML=data.name;
+      if(data.weather[0].icon === "01d" || data.weather[0].icon === "01n")
+      {
+        setWicon(clear_icon);
+      }
+      else if(data.weather[0].icon === "02d" || data.weather[0].icon === "02n")
+      {
+        setWicon(cloud_icon);
+      }
+      else if(data.weather[0].icon === "03d" || data.weather[0].icon === "03n")
+      {
+        setWicon(drizzle_icon);
+      }
+      else if(data.weather[0].icon === "04d" || data.weather[0].icon === "04n")
+      {
+        setWicon(drizzle_icon);
+      }
+      else if(data.weather[0].icon === "09d" || data.weather[0].icon === "09n")
+      {
+        setWicon(rain_icon);
+      }
+      else if(data.weather[0].icon === "10d" || data.weather[0].icon === "10n")
+      {
+        setWicon(rain_icon);
+      }
+      else if(data.weather[0].icon === "13d" || data.weather[0].icon === "13n")
+      {
+        setWicon(snow_icon);
+      }
+      else{
+        setWicon(clear_icon);
+      }
+    }
+    else{
+      //alert("City not found!!");
+      //props.showAlert("City not found !!","warning")
+      
+      err[0].innerHTML="City not Found !! "
+    }
+    }
+    useEffect(() => {
+      search();
+    });
+  return (
+    
+    <div className="container">
+      <div className="top-bar">
+        <input type="text" className="cityInput" placeholder='Search'/>
+        <div className="search-icon" onClick={()=>{search()}}>
+            <img src={search_icon} alt="" />
+        </div>
+      </div>
+      <div className="err-msg"></div>
+      <div className="weather-image">
+        <img src={wicon} alt="" />
+      </div>
+      <div className="weather-temp">24°C</div>
+      <div className="weather-location">London</div>
+      <div className="data-container">
+        <div className="element">
+            <img src={humidity_icon} alt="" className="icon" />
+            <div className="data">
+            <div className="humidity-percent">64%</div>
+            <div className="text">Humidity</div>
+            </div>
+        </div>
+        <div className="element">
+            <img src={wind_icon} alt="" className="icon" />
+            <div className="data">
+            <div className="wind-rate">18 km/hr</div>
+            <div className="text">Wind Speed</div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WeatherApp
